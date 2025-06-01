fe2c5570f95ceecb896bbda18a453f03ad767d81
Pran Krishna kk <prankrishnakk340@gmail.com>
2025-06-01 12:23:08 +0530

    Delete dotnetcore directory

--- dotnetcore/WebApplication1/WebApplication1/Model/Students.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApplication1.Model
-{
-    public class Students
-    {
-        public int Id { get; set; }
-        public string ?Name { get; set; }
-        public int Mark{ get; set; }
-    }
-}
